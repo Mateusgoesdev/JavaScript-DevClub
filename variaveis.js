b36eef0e831bf8b880b24afd06530205d7901315
mateusgoesdev <mateusgoes374@gmail.com>
2025-01-18 09:15:55 -0300

    .

---
+++ variaveis.js
@@ -0,0 +1,25 @@
+// VARIÁVEIS
+ 
+let number2=''
+const number3=''
+
+
+// permitido:
+
+// - Iniciar com $ OU _
+// - Colocar acentos  
+// - Iniciar com letras 
+// - Letras maiúsculas e minúsculas
+
+
+// nao pode:
+
+// - Iniciar com números
+// -colocar espaços
+
+ // Ideal: 
+
+// - Camel Case
+// - Snake case
+// - Sempre em ingles 
+// - Nomes que fazem sentido
