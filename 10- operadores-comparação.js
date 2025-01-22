d5ceec02835910d5a08da95c184f9fed06016cb6
mateusgoesdev <mateusgoes374@gmail.com>
2025-01-22 07:08:03 -0300

    .

---
+++ 10- operadores-comparação.js
@@ -0,0 +1,10 @@
+/* Operadores de Comparação
+
+ ==  -> Igual // Ele compara o valor, mas não compara o tipo do valor
+ === -> Totalmente igual // ele verifica o tipo o dado
+ 
+ != -> Diferente // Ele compara o valor, mas não compara o tipo do valor
+ !== -> Totalmente diferente // Ele verifica o tipo do dado
+ */
+
+ 
