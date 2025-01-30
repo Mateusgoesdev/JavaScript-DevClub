33daa1e6414440519c8d3c652a34dd7a1b612ccd
mateusgoesdev <mateusgoes374@gmail.com>
2025-01-30 07:50:02 -0300

    .

--- Exercicios - JS02/ex03.js
+++ Exercicios - JS02/ex03.js
@@ -1,10 +1,11 @@
 // Faça um programa que gere um número aleatório entre 1 e 10, esse número 'ganhador'. Então ele irá gerar um número aleatóro entre 1 e 10, e verifica se a pessoa ganhou ou não, se for o gahadador, mostrar que venceu e se perder, mostrar que perdeu
 
 numeroSorteado = Math.floor((Math.random() * (10 - 1) + 1)) 
-númeroAleatorio = Math.floor((Math.random() * (10 - 1) + 1))
+numeroAleatorio = Math.floor((Math.random() * (10 - 1) + 1))
 
-if(númeroAleatorio == numeroSorteado) {
-    console.log(`Parabéns, você foi Sorteado!(${numeroSorteado})`)
+console.log(numeroSorteado)
+console.log(numeroAleatorio)
+if(numeroAleatorio == numeroSorteado) {
+console.log(`Voce ganhou`)
 } else {
-    console.log(`O número sorteado desta vez foi ${numeroSorteado}, tente outra vez`)
-}
+    console.log(`Voce perdeu`)}
