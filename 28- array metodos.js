2a1d30b9732ab647420373387dc90f227cbef908
mateusgoesdev <mateusgoes374@gmail.com>
2025-02-13 14:26:02 -0300

    .

---
+++ 28- array metodos.js
@@ -0,0 +1,16 @@
+// Array - Métodos 
+
+const students = ['Joao', 'Maria', 'Pedro', 'Carla', 'Bia']
+
+students.push('Mateus')
+console.log(students)
+
+// Push  -> Adiciona um elemento no final do array
+// Length  -> Mostra o tamanho do array
+// sort  -> Coloca os itens por ordem alfabética
+// delete  -> deleta itens
+
+// Splice (unir, ligar)  -> Tira ou coloca itens em lugares específicos
+// Slice (fatiar)
+// Pop  -> tira o último item do array
+// Shift  -> tira o primeiro item do array  
